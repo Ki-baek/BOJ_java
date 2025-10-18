2c2b2d812dc2bdcb6918274f95b9f14f684e8b98
woora03@naver.com <woora03@naver.com>
2025-10-18 20:40:31 +0900

    commit

--- 2000/2557_Hello_World.java
+++
@@ -1,7 +0,0 @@
-package 2000;
-
-public class 2557_Hello_World{
-    public static void main(String[] args){
-      System.out.println("Hello World")
-    }
-}
---
+++ F2000/Q2557_Hello_World.java
@@ -0,0 +1,7 @@
+package F2000;
+
+public class Q2557_Hello_World{
+    public static void main(String[] args){
+      System.out.println("Hello World!");
+    }
+}
